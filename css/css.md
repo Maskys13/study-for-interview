0d09ba649915fb092bc506028df00546b1d1922a
matiantian <ma_tiantian@topsec.com.cn>
2024-02-29 01:13:48 +0800

    feat: 2024-2-28

--- css/css.md
+++ css/css.md
@@ -6,3 +6,60 @@
 ```
 父元素不用opacity，设置background: rgba()。
 ```
+### 常见的水平垂直居中实现方案
+```css
+/* flex布局 */
+.parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* 绝对定位 */
+.parent {
+  position: relative;
+}
+.child {
+  position: absolute;
+  top: 0; left: 0; right: 0; bottom: 0;
+  margin: auto;
+}
+
+/* 绝对定位配合transform */
+.parent {
+  position: relative;
+}
+.child {
+  position: absolute;
+  top: 50%; left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+```
+### BFC
+```
+BFC 即 Block Formatting Contexts (块级格式化上下文)，通俗一点来讲，可以把 BFC 理解为一个封闭的大箱子，箱子内部的元素无论如何翻江倒海，都不会影响到外部。
+
+1. 根元素
+2. float的值不为none
+3. overflow的值不为visible
+4. display的值为 inline-block/ table-cell/ table-caption/ flex/ inline-flex 
+5. position的值为absolute或fixed
+
+1. Box垂直方向的距离由margin决定，属于同一个BFC的两个相邻box的margin会发生重叠（应用：防止marin重叠）
+2. BFC的区域不会与float box发生重叠（应用：自适应两栏布局）
+3. 计算BFC的高度时，浮动元素也参与计算（应用：清除内部浮动）
+4. BFC内部的Box会在垂直方向，一个接一个的放置。
+5. 每个元素的margin box的左边会与包含块border box的左边相接触（对于从左到右的格式化，否则相反），即使存在浮动也会如此。
+6. BFC就是页面上的一个独立容器，容器里面的元素不会影响到外面的元素
+```
+### flex: 1
+```
+flex: 1; 是flex-grow: 1; flex-shrink: 1; flex-basis: 0%;
+```
+### 隐藏元素的属性
+```css
+display: none;  不占空间
+visibility: hidden; 占空间，内容隐藏，触摸不到
+opacity: 0; 占空间，能触摸到
+```
--- js/index.js
+++ js/index.js
@@ -10,3 +10,142 @@ function func1() {
 }
 
 const b = func1()
+
+class People {}
+class Student extends People {}
+
+const vortesnail = new Student();
+
+console.log(vortesnail instanceof People); // true
+console.log(vortesnail instanceof Student); // true
+
+
+// new功能函数
+function newFunc(...args) {
+  const constructor = args.shift()
+  const newInstance = Object.create(constructor.prototype)
+  const res = constructor.apply(newInstance, args)
+  return (typeof res === 'object' || typeof res === 'function') && res !== null ? res : newInstance
+}
+
+function cons() {
+  this.name = 'lihua'
+  return BigInt(1321321323132123)
+}
+
+const res = new cons()
+console.log(res);
+
+// 继承
+// 1.原型链继承
+function Parent1() {
+  this.age = '20'
+}
+
+Parent1.prototype.sayHello = function() {
+  console.log('原型链继承');
+}
+
+function Child1() {
+  this.name = 'child'
+}
+Child1.prototype = new Parent1()
+
+var child1 = new Child1()
+child1.sayHello()
+console.log(child1.age);
+
+// 2.构造函数继承
+function Parent2(name) {
+  this.name = name
+}
+
+function Child2(name) {
+  Parent2.call(this, name)
+}
+
+var child2 = new Child2('构造函数继承')
+console.log(child2.name);
+
+// 3.组合继承(原型继承和构造函数继承组合)
+function Parent3(name) {
+  this.name = name
+}
+Parent3.prototype.sayHello = function() {
+  console.log('组合继承');
+}
+function Child3(name) {
+  Parent3.call(this, name)
+}
+Child3.prototype = new Parent3()
+
+var child3 = new Child3('组合')
+child3.sayHello()
+console.log(child3);
+
+// 4.原型式继承
+const parent4 = {
+  age: 20,
+  name: '原型继承',
+  sayHello() {
+    console.log(this.name);
+  }
+}
+
+const child4 = Object.create(parent4)
+child4.sayHello()
+
+// 寄生式继承
+function objectCopy(obj) {
+  function Fn() {}
+  Fn.prototype = obj
+  return new Fn()
+}
+
+function createAnother(obj) {
+  let clone = objectCopy(obj)
+  clone.showName = function() {
+    console.log(this.name);
+  }
+  return clone
+}
+
+let person = {
+  age: 10,
+  name: '寄生继承',
+  hobby: ['唱', '跳']
+}
+
+let child5 = createAnother(person)
+console.log(child5.name);
+
+// 寄生组合继承
+function inheritPrototype(child, parent) {
+  let prototype = objectCopy(parent.prototype)
+  prototype.constructor = child
+  child.prototype = prototype
+}
+
+function Parent6(name) {
+  this.name = name
+  this.hobby = ['唱', '跳']
+}
+Parent6.prototype.showName = function() {
+  console.log(this.name);
+}
+
+function Child6(name, age) {
+  Parent6.call(this, name)
+  this.age = age
+}
+
+inheritPrototype(Child6, Parent6)
+Child6.prototype.showAge = function() {
+  console.log(this.age);
+}
+
+let child6 = new Child6('寄生组合继承', 30)
+
+child6.showName()
+child6.showAge()
+
